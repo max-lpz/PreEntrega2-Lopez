3de5168daec5c5aa057389755ca859efaadade47
Maximo Nahuel Lopez <maximonahuellopez@gmail.com>
2023-06-21 10:17:51 -0300

    FirstCommit PreEntrega2

---
+++ js/file.js
@@ -0,0 +1,51 @@
+let productos = [
+    {
+        id: "1",
+        nombre: "Celular",
+        marca: "iPhone",
+        costo: 150000,
+    },
+    {
+        id: "2",
+        nombre: "Bicicleta",
+        marca: "BMX",
+        costo: 80000,
+    },
+    {
+        id: "3",
+        nombre: "Carpa",
+        marca: "Montagne",
+        costo: 360000,
+    },
+    {
+        id: "4",
+        nombre: "Accesorios",
+        marca: "Deco",
+        costo: 5000,
+    },
+]
+
+let resultado = []
+let mensaje = ""
+let idProd = 0
+
+let pedirProducto = () => {
+    idProd = prompt("¿Qué producto desea ver?\n1-Celular\n2-Bicicleta\n3-Carpa\n4-Accesorios\n0-Salir")
+    if (idProd != 0) {
+        let productoElegido = productos.find((producto) => producto.id === idProd)
+        while (!productoElegido) {
+            alert("Producto no encontrado.")
+            idProd = prompt("¿Qué producto desea ver?\n1-Celular\n2-Bicicleta\n3-Carpa\n4-Accesorios\n0-Salir")
+        }
+        return productoElegido
+    } else {
+        alert('Adios, gracias por si visita')
+    }
+}
+
+let producto = pedirProducto()
+resultado.push(producto)
+resultado.forEach((producto) => {
+    mensaje += `El producto: ${producto.nombre} tiene un costo de: $${producto.costo}\n`
+})
+alert(mensaje)
